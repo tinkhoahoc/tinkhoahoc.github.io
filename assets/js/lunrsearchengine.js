a2c1b828d438dbfa020693a3951afee0eb7bb192
Hoang-Long Cao <chlong.mechatronics@gmail.com>
2023-04-17 17:01:45 +0200

    Update

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -26,7 +26,7 @@ var documents = [{
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/ngheo-kho-ve-khoa-hoc/",
+    "url": "http://localhost:4000/ngheo-kho-va-khoa-hoc/",
     "title": "Sinh ra trong nghèo khó hạn chế sự nghiệp khoa học",
     "body": "2023/04/17 - Kiến thức chuyên môn chưa bao giờ là đủ để đi trên con đường khoa học chuyên nghiệp. Khi bạn sinh ra trong hoàn cảnh khó khăn về tài chính và văn hóa, việc giao tiếp và xây dựng mối quan hệ trong cộng đồng khoa học trở nên vô cùng khó khăn. Tuy nhiên, không phải ai cũng có được sự tự tin để kể ra câu chuyện của mình như Haili Hu - một nhà khoa học gốc Trung Quốc ở Hà Lan. Trên tạp chí Science, cô đã chia sẻ về những khó khăn mà cô phải đối mặt khi bước vào thế giới khoa học chuyên nghiệp. Haili Hu - nhà khoa học gốc Trung Quốc ở Hà Lan đã kể lại câu chuyện của mình trên tạp chí Nature.  “Thông minh là chưa đủ. ”  Người ta nói chuyện về chính trị, triết học và văn hóa. Gia đình tôi không bao giờ trò chuyện về những vấn đề này. Cô cảm thấy không tự tin và không cảm thấy mình là một phần của cuộc chơi. Dù có sự trợ giúp của cấp trên và cảm giác tiêu cực được giảm bớt đi phần nào nhưng vấn đề vẫn còn đó. Cô nhận ra rằng nền tảng gia đình và văn hóa đã ảnh hưởng không tốt đến sự nghiệp của mình nhiều hơn cô nghĩ. Liệu khoa học chỉ dành riêng cho những người giàu? Có bao nhiêu câu chuyện tương tự chưa được kể?  Hu, H. , 2023. Embracing my past. Science (New York, NY), 379(6636), pp. 1062-1062. "
     }, {
