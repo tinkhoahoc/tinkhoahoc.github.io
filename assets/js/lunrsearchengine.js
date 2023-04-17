1e8f0cf0848f31544f54cb2f2250da5c23ace1a2
Hoang-Long Cao <chlong.mechatronics@gmail.com>
2023-04-17 16:59:30 +0200

    Update

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "http://localhost:4000/about",
     "title": "Thông tin khoa học mỗi ngày cùng Lumina",
-    "body": " Scientia vincere tenebras - Khoa học chinh phục bóng tối Càng đọc các nghiên cứu khoa học tôi càng nhận ra mình không biết gì và đây là nơi tôi lưu lại sự ngu dốt của mình. Tôi không có tham vọng đem nghiên cứu đến cho mọi người mà chỉ muốn chia sẻ với các bạn những gì tôi cảm thấy thích thú. Tôi luôn luôn đọc nghiên cứu gốc và không dựa vào thông tin trung gian của bên thứ ba. Vì vậy việc đôi lúc tôi hiểu sai hay diễn dạt không chính xác là không thể tránh khỏi. Nếu bạn có thời gian, hãy đọc thêm nghiên cứu gốc có thêm thông tin chi tiết và chính xác nhất! Buy me a coffee Đừng để đứa trẻ tò mò trong mỗi chúng ta lớn lên! "
+    "body": " Scientia vincere tenebras - Khoa học chinh phục bóng tối Càng đọc các nghiên cứu khoa học tôi càng nhận ra mình không biết gì và đây là nơi tôi lưu lại sự ngu dốt của mình. Tôi không có tham vọng đem nghiên cứu đến cho mọi người mà chỉ muốn chia sẻ với các bạn những gì tôi cảm thấy thích thú. Tôi luôn luôn đọc nghiên cứu gốc và không dựa vào thông tin trung gian của bên thứ ba. Vì vậy việc đôi lúc tôi hiểu sai hay diễn dạt không chính xác là không thể tránh khỏi. Nếu bạn có thời gian, hãy đọc thêm nghiên cứu gốc có thêm thông tin chi tiết và chính xác nhất! Đừng để đứa trẻ tò mò trong mỗi chúng ta lớn lên! "
     }, {
     "id": 2,
     "url": "http://localhost:4000/categories",
